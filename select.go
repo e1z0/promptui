499ea51ab5e65a92508160efbec0cd8f33aafe74
e1z0 <7213361+e1z0@users.noreply.github.com>
2025-02-25 23:37:02 +0200

    Update select.go

--- select.go
+++ select.go
@@ -590,7 +590,7 @@ func (s *Select) setKeys() {
 		PageUp:   Key{Code: KeyBackward, Display: KeyBackwardDisplay},
 		PageDown: Key{Code: KeyForward, Display: KeyForwardDisplay},
 		Search:   Key{Code: '/', Display: "/"},
-		Exit:     Key{Code: 'Esc', Display: "Esc"},
+		Exit:     Key{Code: readline.CharEsc, Display: "Esc"},
 	}
 }
 
